68f382aecdee4d902f264939fe43dd0fee0e18eb
tvu <thanhauco@gmail.com>
2018-01-19 05:46:51 -0800

    Feat Services/Api:  Adding 4 files including ReportDefTests.cs

---
+++ AssetGuard.Tests/Services/Reporting/ReportExecutionTests.cs
@@ -0,0 +1,2 @@
+using Xunit;
+namespace AssetGuard.Tests.Services.Reporting { public class ReportExecutionTests { [Fact] public void Test1() { Assert.True(true); } } }
---
+++ AssetGuard.Tests/Services/Reporting/ReportScheduleTests.cs
@@ -0,0 +1,2 @@
+using Xunit;
+namespace AssetGuard.Tests.Services.Reporting { public class ReportScheduleTests { [Fact] public void Test1() { Assert.True(true); } } }
