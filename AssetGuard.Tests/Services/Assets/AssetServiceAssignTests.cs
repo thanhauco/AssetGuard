966251413f5da7ee015e12987be274a1f38a5fb5
tvu <thanhauco@gmail.com>
2018-01-26 09:01:36 -0800

    Feat Services/Api:  Adding 4 files including AssetServiceAssignTests.cs

---
+++ AssetGuard.Tests/Services/Assets/AssetServiceAssignTests.cs
@@ -0,0 +1,2 @@
+using Xunit;
+namespace AssetGuard.Tests.Services.Assets { public class AssetServiceAssignTests { [Fact] public void Test() { } } }
---
+++ AssetGuard.Tests/Services/Issues/IssueServiceReportTests.cs
@@ -0,0 +1,2 @@
+using Xunit;
+namespace AssetGuard.Tests.Services.Issues { public class IssueServiceReportTests { [Fact] public void Test() { } } }
